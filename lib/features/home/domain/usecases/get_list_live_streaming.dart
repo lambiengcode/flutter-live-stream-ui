5ddbdd7184e7a023cc1b540ed409525f5102caf0
lambiengcode <lambiengcode@gmail.com>
2022-10-07 14:06:09 +0700

    config(): build example injectable_generator

--- lib/features/home/domain/usecases/get_list_live_streaming.dart
+++ lib/features/home/domain/usecases/get_list_live_streaming.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:streamskit_mobile/core/error/failure.dart';
 import 'package:streamskit_mobile/core/usecase/usecase.dart';
 import 'package:streamskit_mobile/features/home/data/model/live_stream_model.dart';
 import 'package:streamskit_mobile/features/home/domain/repositories/live_stream_repository.dart';
 
+@lazySingleton
 class GetListLiveStreaming implements UseCase<List<LiveStreamModel>, NoParams> {
   final LiveStreamRepository repository;
 
